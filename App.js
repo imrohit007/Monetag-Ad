82551e1776834f3bb89b249a0304cf2ce289544f
imrohit007 <rohitthakur9830@gmail.com>
2023-11-07 19:17:36 +0530

    ninza7

--- App.js
+++ App.js
@@ -1,10 +1,41 @@
+import 'expo-dev-client';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Notix from 'notix-rn';
+import React, { useEffect } from 'react';
 
 export default function App() {
+  let interstitialLoader;
+
+  useEffect(() => {
+    loadInterstitial();
+
+    initializePushNotifications();
+  }, []);
+
+  const loadInterstitial = async () => {
+    interstitialLoader = await Notix.Interstitial.createLoader(###########);
+    interstitialLoader.startLoading();
+
+    try {
+      var interstitialData = await interstitialLoader.next(5000);
+    } catch (Exception) {
+      return;
+    }
+
+    Notix.Interstitial.show(interstitialData);
+  };
+
+  const initializePushNotifications = () => {
+    const notixAppId = '#########';
+    const notixToken = '################';
+
+    Notix.Notification.init(notixAppId, notixToken);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Testing Monetag Ad Code</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +48,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  text:{
+      fontSize: 20,
+      fontWeight: 'bold'
+  }
 });
